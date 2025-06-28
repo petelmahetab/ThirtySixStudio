41e18cc419093c7e1ec0954dd120e4f5d3892135
Mahetab Patel <patelmahetab9020@gmail.com>
2025-06-28 15:49:39 +0530

    Added

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
 import canvasImages from '../CanvasImages'
-import { useEffect,useRef,useState } from 'react'
-import {useGSAP} from '@gsap/react';
-import gsap from 'gsap';
-// Load 150 images here p. You need to create the ref using useRef() which you've already imported
-export default function Canvas({details}) {
-    const {startIndex,numImages,duration,size,top,left,zIndex}=details;
-    const [index,setIndex]=useState({value:details.startIndex})
-    const canvasRef=useRef(null)
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-    useGSAP(()=>{
-        gsap.to(index,{
-            value:startIndex+numImages-1,//150-1=149
-            duration:duration,
-            ease:"linear",
-            repeat:-1,
-            onUpdate:()=>{
-                setIndex({value: Math.round(index.value)}) // Fixed syntax
-            },
-        })
-    })
-    useEffect(() => {
-        const scale = window.devicePixelRatio;
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        const img = new Image();
-        img.src = canvasImages[index.value];
-        img.onload = () => {
-          canvas.width = canvas.offsetWidth * scale;
-          canvas.height = canvas.offsetHeight * scale;
-          canvas.style.width = canvas.offsetWidth + "px";
-          canvas.style.height = canvas.offsetHeight + "px";
-    
-          ctx.scale(scale, scale);
-          ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
-        };
-      }, [index]);
-    //Array means that the useEffect will run only once when the component is mounted
-    return (
-        <canvas 
-          ref={canvasRef}
-          id="Canvas"
-          className='absolute'
-         style={{weight:`${size*1.8}px`,
-          height:`${size*1.8}px`,
-          top:`${top}%`,
-          left:`${left}%`,
-          zIndex:`${zIndex}`
-        }}
-        />
-      )
-   
-  }  
+function Canvas({ details }) {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
 
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+
+    gsap.from(canvasRef.current, {
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
+
+  useEffect(() => {
+    const scale = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    const imgSrc = canvasImages[index.value];
+    console.log('Loading image:', imgSrc);
+    img.src = imgSrc;
+  
+    img.onload = () => {
+      console.log('Image loaded');
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    };
+  }, [index]);
+  
+  return (
+    <canvas
+      data-scroll
+      data-scroll-speed={Math.random().toFixed(1)}
+      ref={canvasRef}
+      className="absolute"
+      style={{
+        width: `${size * 1.8}px`,
+        height: `${size * 1.8}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      id="canvas"
+    ></canvas>
+  );
+}
+
+export default Canvas;
